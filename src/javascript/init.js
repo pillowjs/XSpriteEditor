5c126fe7bc603d3875c731c80d79e6f16861591c
xudafeng <xudafeng@126.com>
2017-06-21 11:55:44 +0800

    rename

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -1,6 +1,6 @@
 //aml config
 ;aml.config({
-    path: (false ? 'http://rawgithub.com/xudafeng/XSpriteEditor/master/src/javascript/' : 'src/javascript/')
+    path: (false ? '//pillowjs.github.io/XSpriteEditor/src/javascript/' : 'src/javascript/')
 });
 ;define('init',['stage','preview','timer','status'],function(Stage,PreView,Timer,Status){
     var classQuery = arguments;
